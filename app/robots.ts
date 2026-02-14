676a5ae1e9c63e16de4963d907746aa53a8afbe1
Nitish Kumar S <nknitishkumara@gmail.com>
2026-02-15 04:34:07 +0530

    Robots update

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.hopeandhealphysio.com'
+    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://hopeandhealphysio.com'
 
     return {
         rules: {
